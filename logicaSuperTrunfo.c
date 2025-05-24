c97bbc25e500f2ddae38b97ffa2938b60f9806fe
ProgramacaoGH <wevanyfrancisco@gmail.com>
2025-05-24 18:00:36 -0300

    Atualizar o logicaSuperTrunfo.c

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -2,86 +2,80 @@
 
 int main(){
     // informaçoes necessarias para as cartas do jogo.
-    int carta1 = 1;
+    int carta1= 1;
     int carta2 = 2;
-    char estado1[50];
-    char estado2[50];
-    char codigo1;
-    char codigo2;
-    char cidade1[8];
-    char cidade2[8]
-    int populacao1;
-    int populacao2;
-    float area1;
-    float area2;
-    float pib1;
-    float pib2;
-    int pontoTuristico1;
-    int pontoTuristico2;
+    char estado[50];
+    char codigo;
+    char cidade[8];
+    int populacao;
+    float area;
+    float pib;
+    int pontoTuristico;
+    double densidadePopulacional, pibPercapita;
 
     //pedindo as informaçoes ao jogador 1 para imprimir o resultado na tela.
     printf("Digite o Estado 1: \n");
-    scanf("%s", &estado1);
+    scanf("%s", &estado);
 
     printf("Digite o Código 1: \n");
-    scanf("%i", &codigo1);
+    scanf("%i", &codigo);
 
     printf("Digite a Cidade 1: \n");
-    scanf("%s", &cidade1);
+    scanf("%s", &cidade);
 
     printf("Digite a População 1: \n");
-    scanf("%d", &populacao1);
+    scanf("%d", &populacao);
 
     printf("Digite a Área 1: \n");
-    scanf("%f", &area1);
+    scanf("%f", &area);
 
     printf("Digite o PIB 1: \n");
-    scanf("%d", &pib1);
+    scanf("%d", &pib);
 
     printf("Quantidade de Ponto Turisticos 1: \n");
-    scanf("%d", &pontoTuristico1);
+    scanf("%d", &pontoTuristico);
 
     // imprimindo dados da carta do primeiro jogador na tela.
-    printf("Carta: %i\n", carta1);
-    printf("Estado: %s\n", estado1);
-    printf("Código: %i\n", codigo1);
-    printf("Cidade: %s\n", cidade1);
-    printf("População: %d\n", populacao1);
-    printf("Área: %f\n", area1);
-    printf("PIB: %d\n", pib1);
-    printf("Quantida de Ponto Turistico: %d\n", pontoTuristico1);
+    printf("Carta1: %i\n", carta1);
+    printf("Estado: %s\n", estado);
+    printf("Código: %i\n", codigo);
+    printf("Cidade: %s\n", cidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %f\n", area);
+    printf("PIB: %d\n", pib);
+    printf("Quantida de Ponto Turistico: %d\n", pontoTuristico);
 
     // pedindo as informaçoes ao jogador 2 para imprimir na tela.
     printf("Digite o Estado 2: \n");
-    scanf("%s", &estado2);
+    scanf("%s", &estado);
 
     printf("Digite o Código 2: \n");
-    scanf("%i", &codigo2);
+    scanf("%i", &codigo);
 
     printf("Digite a Cidade 2: \n");
-    scanf("%s", &cidade2);
+    scanf("%s", &cidade);
 
     printf("Digite a População 2: \n");
-    scanf("%d", &populacao2);
+    scanf("%d", &populacao);
 
     printf("Digite a Área 2: \n");
-    scanf("%f", &area2);
+    scanf("%f", &area);
 
     printf("Digite o PIB 2: \n");
-    scanf("%d", &pib2);
+    scanf("%d", &pib);
 
     printf("Quantidade de Ponto Turisticos 2: \n");
-    scanf("%d", &pontoTuristico2);
+    scanf("%d", &pontoTuristico);
 
     // imprimindo dados da carta do jogador 2 na tela.
-    printf("Carta2: %i\n", carta2);
-    printf("Estado: %s\n", estado2);
-    printf("Código: %i\n", codigo2);
-    printf("Cidade: %s\n", cidade2);
-    printf("População: %d\n", populacao2);
-    printf("Área: %f\n", area2);
-    printf("PIB: %d\n", pib2);
-    printf("Quantida de Ponto Turistico: %d\n", pontoTuristico2);
+    printf("Carta2: %i\n", carta);
+    printf("Estado: %s\n", estado);
+    printf("Código: %i\n", codigo);
+    printf("Cidade: %s\n", cidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %f\n", area);
+    printf("PIB: %d\n", pib);
+    printf("Quantida de Ponto Turistico: %d\n", pontoTuristico);
 
     return 0;
 } 
@@ -110,6 +104,9 @@ int main() {
     // Comparação de Cartas:
     // Desenvolva a lógica de comparação entre duas cartas.
     // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+
+    densidadePopulacional = populacao / area;
+    pibPercapita = pib / polulacao;
 
     // Exemplo:
     if (populacao1 > populacao2) {
